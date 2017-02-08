184c8fad985b00cad1c41fa2ecc2c8141e9cba9d
Yemi Oladimeji <b4030796@newcastle.ac.uk>
2017-02-08 12:14:41 +0000

    Completed SUVAT header.

--- SUVATCalc/SUVATCalc/SUVAT.h
+++ SUVATCalc/SUVATCalc/SUVAT.h
@@ -1,2 +1,25 @@
 #pragma once
-#include ""
+#include "Vector3D.h"
+#ifndef SUVAT_H
+#define SUVAT_H
+
+
+class SUVAT {
+
+private:
+	Vector3D u;
+	Vector3D a;
+	float t;
+
+public:
+	//Constructors
+	SUVAT(); //Default constructor
+	~SUVAT() {}; //Destructor 
+	
+	SUVAT(const SUVAT &s); //Copy constructor
+	const SUVAT	operator= (const SUVAT &s); //Assignment operator
+
+	inline void setTime();
+	inline float getTime();
+};
+#endif //SUVAT_H
