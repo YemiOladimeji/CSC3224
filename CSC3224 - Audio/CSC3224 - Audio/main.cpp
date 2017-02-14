7e8f8b418d74f914917cb2932177376ff99b2243
Yemi Oladimeji <b4030796@newcastle.ac.uk>
2017-02-14 13:42:09 +0000

    Added error checking for the music.

--- CSC3224 - Audio/CSC3224 - Audio/main.cpp
+++ CSC3224 - Audio/CSC3224 - Audio/main.cpp
@@ -1,14 +1,21 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace std;
 
 int main() {
 	sf::Music music;
 	sf::RenderWindow window(sf::VideoMode(400, 400), "Best Girl Simulator 2017");
 	sf::Event event;
 
-	music.openFromFile("Music\\Rem Voice Remix - Re-Zero.flac"); //Load in music file to play as BGM
-	music.play(); //Play music
+	if (!music.openFromFile("Music\\Rem Voice Remix - Re-Zero.flac")) {
+		cout << "Music file could not be located.";
+	}
+	else {
+		music.play();
+	}
 
 	while(window.isOpen()){ //Open window
 		while (window.pollEvent(event)) {
