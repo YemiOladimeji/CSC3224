474ff6367b0acc7e4dc3a013b2af4d239b50b730
Yemi Oladimeji <b4030796@newcastle.ac.uk>
2017-02-14 15:03:27 +0000

    Used a temporary image loading solution. Need to look into implementing my own.

--- CSC3224 - Audio/CSC3224 - Audio/main.cpp
+++ CSC3224 - Audio/CSC3224 - Audio/main.cpp
@@ -7,23 +7,28 @@
 using namespace std;
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode(400, 400), "Best Girl Simulator 2017");
+	sf::RenderWindow window(sf::VideoMode(400, 440), "Best Girl Simulator 2017");
 	sf::Event event;
 	AudioControl aC;
-	GraphicsControl gC;
+	//GraphicsControl gC;
 
 	aC.loadBGM("Music\\Rem Voice Remix - Re-Zero.flac");
-	
+
+	//Temp image loading solution
+	sf::Texture img;
+	if (!img.loadFromFile("Images\\Rem.png")) return 0;
+	sf::Sprite sprite;
+	sprite.setTexture(img);
 
 	while(window.isOpen()){ //Open window
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed) {
 				window.close();
 			}
+			window.clear(sf::Color::White);
+			window.draw(sprite);
+			window.display();
 		}
-		window.clear();
-		window.draw(gC.setSpriteTexture(gC.loadTexture("Images\\dance.gif")));
-		window.display();
 	}
 	return 0;
 }
