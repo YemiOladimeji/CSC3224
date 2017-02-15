75976751c033474d71cd54783efae603b4d5cd01
Yemi Oladimeji <b4030796@newcastle.ac.uk>
2017-02-15 13:01:17 +0000

    Beginning the abstraction process.

---
+++ CSC3224 - Game Development Project/CSC3224 - Initialise and Shutdown/main.cpp
@@ -0,0 +1,18 @@
+#include <SFML/Window.hpp>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	sf::Window window(sf::VideoMode(600, 600), "Best Girl Simulator 2017");
+	sf::Event event;
+
+	while (window.isOpen()) { //Open window
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+	}
+	return 0;
+}
