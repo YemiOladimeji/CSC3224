f04c04982d5892bc5422c44bc41b5ec22d682f7c
Yemi Oladimeji <b4030796@newcastle.ac.uk>
2017-03-01 12:32:05 +0000

    Abstracting the SFML Graphics module.

---
+++ CSC3224 - CGD/Graphics/CSC3224Graphics.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <SFML\Graphics.hpp>
+
+class CSC3224Graphics {
+
+	void clearWindow(sf::RenderWindow w);
+	void displayWindow(sf::RenderWindow w);
+	void closeWindow(sf::RenderWindow w);
+
+};
--- CSC3224 - CGD/Initialise and Shutdown/main.cpp
+++ CSC3224 - CGD/Initialise and Shutdown/main.cpp
@@ -1,19 +1,7 @@
-#include <SFML/Window.hpp>
 #include <iostream>
 
 using namespace std;
 
 int main() {
-	sf::Window window(sf::VideoMode(720, 480), "CSC3224 - Game Development Project");
-	sf::Event event;
-
-	while (window.isOpen()) {
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed) {
-				window.close();
-			}
-		}
-		//Game loop will go here
-	}
 	return 0;
 }
