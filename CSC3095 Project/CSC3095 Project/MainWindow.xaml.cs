4ace213a48f5b8f11f575ae6071cb81084141f29
Yemi <theyemi@hotmail.com>
2017-03-03 01:20:28 +0000

    Removing non-functional Body Tracking code.

--- CSC3095 Project/CSC3095 Project/MainWindow.xaml.cs
+++ CSC3095 Project/CSC3095 Project/MainWindow.xaml.cs
@@ -25,10 +25,6 @@ namespace CSC3095_Project
     {
         private KinectSensor kinect;
         private MultiSourceFrameReader reader;
-        //private CoordinateMapper cM = null; //Coordinate mapper to map one type of point to another
-        private BodyFrameReader bFR = null;
-        private IList<Body> bodies = null; //Array for the bodies
-        //private List<Tuple<JointType, JointType>> bones; //Definition of bones
         private Gesture waveGesture;
         private VisualGestureBuilderFrameSource gestureSource;
         private VisualGestureBuilderFrameReader gestureReader;
@@ -39,7 +35,6 @@ namespace CSC3095_Project
             initialiseKinect();
             openReaders();
             loadGesture();
-            openBodyReader();
             openGestureReader();
         }
 
@@ -62,18 +57,8 @@ namespace CSC3095_Project
 
         void openReaders()
         {
-            reader = kinect.OpenMultiSourceFrameReader(FrameSourceTypes.Color | FrameSourceTypes.Body);
+            reader = kinect.OpenMultiSourceFrameReader(FrameSourceTypes.Color);
             reader.MultiSourceFrameArrived += Reader_MultiSourceFrameArrived;
-        }
-        
-        void openBodyReader()
-        {
-            if (this.bodies == null)
-            {
-                this.bodies = new Body[this.kinect.BodyFrameSource.BodyCount];
-            }
-            this.bFR = this.kinect.BodyFrameSource.OpenReader();
-            this.bFR.FrameArrived += OnBodyFrameArrived;
         }
 
         void openGestureReader()
@@ -100,40 +85,12 @@ namespace CSC3095_Project
                 this.gestureSource.TrackingIdLost -= this.OnTrackingIdLost;
                 this.gestureSource.Dispose();
             }
-            this.bFR.Dispose();
-            this.bFR = null;
         }
 
         void OnTrackingIdLost(object sender, TrackingIdLostEventArgs e)
         {
             this.gestureReader.IsPaused = true;
             this.detectBlock.Text = "There's no-one here...";
-        }
-
-        void OnBodyFrameArrived(object sender, BodyFrameArrivedEventArgs e)
-        {
-            using (var frame = e.FrameReference.AcquireFrame())
-            {
-                if (frame != null)
-                {
-                    frame.GetAndRefreshBodyData(this.bodies);
-
-                    var trackedBody = this.bodies.Where(b => b.IsTracked).FirstOrDefault();
-
-                    if (trackedBody != null)
-                    {
-                        if (this.gestureReader.IsPaused)
-                        {
-                            this.gestureSource.TrackingId = trackedBody.TrackingId;
-                            this.gestureReader.IsPaused = false;
-                        }
-                    } else
-                    {
-                        this.OnTrackingIdLost(null,null);
-                    }
-
-                }
-            }
         }
 
         void OnGestureFrameArrived(object sender, VisualGestureBuilderFrameArrivedEventArgs e)
@@ -165,87 +122,9 @@ namespace CSC3095_Project
                     Camera.Source = ToBitmap(frame);
                 }
             }
-
-            //Body stream
-            /*using (var frame = reference.BodyFrameReference.AcquireFrame())
-            {
-                if (frame != null)
-                {
-                    bodies = new Body[frame.BodyFrameSource.BodyCount];
-
-                    frame.GetAndRefreshBodyData(bodies);
-
-                    foreach (var body in bodies)
-                    {
-                        if (body != null)
-                        {
-                            if (body.IsTracked)
-                            {
-                                Joint handRight = body.Joints[JointType.HandRight];
-                                Joint thumbRight = body.Joints[JointType.ThumbRight];
-                                Joint handLeft = body.Joints[JointType.HandLeft];
-                                Joint thumbLeft = body.Joints[JointType.ThumbLeft];
-
-                                canvas.DrawPoint(handRight);
-                                canvas.DrawPoint(handLeft);
-                                canvas.DrawPoint(thumbRight);
-                                canvas.DrawPoint(thumbLeft);
-
-                                // Find the hand states
-                                string rightHandState = "-";
-                                string leftHandState = "-";
-
-                                switch (body.HandRightState)
-                                {
-                                    case HandState.Open:
-                                        rightHandState = "Open";
-                                        break;
-                                    case HandState.Closed:
-                                        rightHandState = "Closed";
-                                        break;
-                                    case HandState.Lasso:
-                                        rightHandState = "Lasso";
-                                        break;
-                                    case HandState.Unknown:
-                                        rightHandState = "Unknown...";
-                                        break;
-                                    case HandState.NotTracked:
-                                        rightHandState = "Not tracked";
-                                        break;
-                                    default:
-                                        break;
-                                }
-
-                                switch (body.HandLeftState)
-                                {
-                                    case HandState.Open:
-                                        leftHandState = "Open";
-                                        break;
-                                    case HandState.Closed:
-                                        leftHandState = "Closed";
-                                        break;
-                                    case HandState.Lasso:
-                                        leftHandState = "Lasso";
-                                        break;
-                                    case HandState.Unknown:
-                                        leftHandState = "Unknown...";
-                                        break;
-                                    case HandState.NotTracked:
-                                        leftHandState = "Not tracked";
-                                        break;
-                                    default:
-                                        break;
-                                }
-                                txtBlock.Text = rightHandState;
-                                txtBlock.Text = leftHandState;
-                                handCheck(handLeft, handRight);
-                            }
-                        }
-                    }
-                }
-            }*/
         }
 
+        //Taking the colour frames from the Kinect and turning them into bitmaps 
         private ImageSource ToBitmap(ColorFrame frame)
         {
             int width = frame.FrameDescription.Width;
