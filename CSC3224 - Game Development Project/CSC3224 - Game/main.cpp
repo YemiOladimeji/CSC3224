071a597f68876e1e8613bed7705f13b8fc86f7b4
Yemi Oladimeji <b4030796@newcastle.ac.uk>
2017-03-07 17:38:20 +0000

    Basic Game Loop implemented.

--- CSC3224 - Game Development Project/CSC3224 - Game/main.cpp
+++ CSC3224 - Game Development Project/CSC3224 - Game/main.cpp
@@ -3,12 +3,15 @@
 int main() {
 	sf::RenderWindow renderWindow(sf::VideoMode(720, 480), "CSC3224 - GameDev Project");
 	sf::Event event;
+	sf::Clock clock;
 
 	while (renderWindow.isOpen()){
 		while (renderWindow.pollEvent(event)) {
 			if (event.type == sf::Event::EventType::Closed) {
 				renderWindow.close();
 			}
+
+			clock.restart();
 			renderWindow.clear();
 			renderWindow.display();
 		}
