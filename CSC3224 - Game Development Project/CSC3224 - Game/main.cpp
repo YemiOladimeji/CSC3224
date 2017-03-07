a9286946bb55477bf815c608920732d98dbb3aea
Yemi Oladimeji <b4030796@newcastle.ac.uk>
2017-03-07 17:30:29 +0000

    Starting to implement the Game Loop.

--- CSC3224 - Game Development Project/CSC3224 - Game/main.cpp
+++ CSC3224 - Game Development Project/CSC3224 - Game/main.cpp
@@ -1,10 +1,16 @@
-#include <iostream>
-
-using namespace std;
-
-int x;
+#include "SFML\Graphics.hpp"
 
 int main() {
-	cout << "Hello, world!";
-	cin >> x;
+	sf::RenderWindow renderWindow(sf::VideoMode(720, 480), "CSC3224 - GameDev Project");
+	sf::Event event;
+
+	while (renderWindow.isOpen()){
+		while (renderWindow.pollEvent(event)) {
+			if (event.type == sf::Event::EventType::Closed) {
+				renderWindow.close();
+			}
+			renderWindow.clear();
+			renderWindow.display();
+		}
+	}
 }
