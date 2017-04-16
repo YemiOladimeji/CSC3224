9f2787c13fbe6c512243bd82490afa1b0c09b72b
Yemi <theyemi@hotmail.com>
2017-04-16 20:01:34 +0100

    Added new GestureDetector class.

---
+++ CSC3095 - Project/CSC3095 - Project/GestureDetector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC3095___Project
+{
+    class GestureDetector : IDisposable
+    {
+
+
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
