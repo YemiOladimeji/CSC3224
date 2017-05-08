4aae471e669b401b1b7e0d6e98c02f024a288815
Yemi Oladimeji <b4030796@newcastle.ac.uk>
2017-05-08 21:42:52 +0100

    Commented out main class.

--- CSC3222 - GameSims/CSC3222 - GameSims/main.cpp
+++ CSC3222 - GameSims/CSC3222 - GameSims/main.cpp
@@ -3,31 +3,38 @@
 
 int main() {
 
-	sf::RenderWindow window(sf::VideoMode(600, 600), "CSC3222 - Gaming Simulaions"); //Initialise the 
-	sf::Sprite bg;
-	sf::Texture mapTexture;
+	sf::RenderWindow window(sf::VideoMode(600, 600), "CSC3222 - Gaming Simulaions"); //Initialise the window
+	sf::Sprite bg; //Instantiate the Sprite object
+	sf::Texture mapTexture; //Instantiate the Texture object
 
-	mapTexture.loadFromFile("Resources\\CSC3222 Coursework.bmp");
-	bg.setTexture(mapTexture);
-	bg.setOrigin(10,10);
-	bg.setPosition(window.getSize().x/2, window.getSize().y/2);
-	bg.scale(30,30);
+	mapTexture.loadFromFile("Resources\\CSC3222 Coursework.bmp"); //Load an image as a texture file
+	bg.setTexture(mapTexture); //Set the Sprite's texture
+	bg.setOrigin(10,10); //Set the origin of the Sprite
+	bg.setPosition(window.getSize().x/2, window.getSize().y/2); //Set the position of the Sprite
+	bg.scale(30,30); //Scale the Sprite up to the size of the Window
 
-	while (window.isOpen()) {
-		sf::Event event;
-		while (window.pollEvent(event)) {
-			if (event.type == event.Closed) {
-				window.close();
+	//While the Window is open
+	while (window.isOpen()) 
+	{
+		sf::Event event; //Instantiate an Event object
+
+		while (window.pollEvent(event)) //While the window is listening for Events
+		{
+			if (event.type == event.Closed) //If the close button is pressed
+			{
+				window.close(); //Close the Window
 			}
-			else if (event.type == event.KeyPressed) {
-				if (event.key.code == sf::Keyboard::K) {
-					//loadSoldiers();
+			else if (event.type == event.KeyPressed) //If a KeyPressed event is triggered
+			{
+				if (event.key.code == sf::Keyboard::K) //If the KeyPressed was the 'K' key
+				{
+					//loadSoldiers(); //Call the loadSoldiers function
 				}
 			}
 		}
-		window.clear();
-		window.draw(bg);
-		window.display();
+		window.clear(); //Clear the window of any existing items
+		window.draw(bg); //Draw the background map
+		window.display(); //Display what is in the current buffer
 	}
 
 }
